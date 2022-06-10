f48d88fb697accc2eca588c38a68a37bf1cfac9e
user <user@users-MBP.lan>
2022-06-10 17:16:21 +0100

    U_R updated

---
+++ console.py
@@ -0,0 +1,14 @@
+import pdb
+from models.transaction import Transaction
+from models.merchant import Merchant
+from models.tag import Tag
+from models.user import User
+
+
+import repositories.transaction_repository as task_repository
+import repositories.user_repository as user_repository
+import repositories.merchant_repository as merchant_repository
+import repositories.tag_repository as tag_repository
+
+
+pdb.set_trace()
