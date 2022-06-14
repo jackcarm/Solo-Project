24137c73ed686a4a51a109a253f83499f3836016
user <user@users-MBP.lan>
2022-06-14 17:26:10 +0100

    MVP updated

--- repositories/merchant_repository.py
+++ repositories/merchant_repository.py
@@ -48,7 +48,7 @@ def delete(id):
 
 
 def update(merchant):
-    sql = "UPDATE merchants SET (name) = (%s) WHERE id = %s"
+    sql = "UPDATE merchants SET name = %s WHERE id = %s"
     values = [merchant.name, merchant.id]
     run_sql(sql, values)
 
